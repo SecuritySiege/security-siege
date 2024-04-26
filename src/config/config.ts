e3e8a73bad82ebaa31534a5c173af8d3629b70d2
OwenPotent <sheanowen3@gmail.com>
2024-04-26 10:22:34 +0800

    Delete src/config/config.ts
    Prevent data breach

--- src/config/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-    ownerIDs: ["730334609841324063"],
-    mongoURI: "mongodb+srv://owen:owen1234@security-siege.es3osyv.mongodb.net/?retryWrites=true&w=majority&appName=Security-Siege",
-}
